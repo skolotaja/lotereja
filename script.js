1b8445ffc47e273c1614f8ad45cbacf3a0f1b32d
skolotaja <94806629+skolotaja@users.noreply.github.com>
2021-11-28 23:59:08 +0200

    lotereja

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ const vardi = ['Jana Krūmiņa', 'Jānis Bērziņš', 'Pēteris Liepa', 'Laura O
 const balvas = ['Dators', 'Telefons', 'Ledusskapis', 'Fotoaparāts', 'Kafijas automāts'];
 const naudaKopa = 1000000;
 let uzvaretajuSkaits = 3;
-let rand = Math.random() * 5;
-rand = Math.floor(rand);
+let rindas = document.querySelector('.rindas');
+//rindas.innerHTML = 'Hello';
 
-console.log(vardi[rand]);
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+    let rand = Math.random() * vardi.length;
+    rand = Math.floor(rand);
+
+
+    let uzvaretajs=vardi[rand];
+    rindas.innerHTML += `
+    <tr>
+        <td>${i+1}</td>
+        <td>${uzvaretajs}</td>
+    </tr>`
+
+
+}
