bbefbe1d647ccf247293314c6c5d2ef7a10aa9a4
skolotaja <94806629+skolotaja@users.noreply.github.com>
2021-11-28 22:39:20 +0200

    lotereja

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert(1)
+const vardi = ['Jana Krūmiņa', 'Jānis Bērziņš', 'Pēteris Liepa', 'Laura Ose', 'Artis Priede'];
+const balvas = ['Dators', 'Telefons', 'Ledusskapis', 'Fotoaparāts', 'Kafijas automāts'];
+const naudaKopa = 1000000;
+let uzvaretajuSkaits = 3;
+let rand = Math.random() * 5;
+rand = Math.floor(rand);
+
+console.log(vardi[rand]);
