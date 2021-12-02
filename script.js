7f6aa29ee0533db7cb377bc8cd8349fc818e708c
skolotaja <94806629+skolotaja@users.noreply.github.com>
2021-12-03 00:23:19 +0200

    lotereja

--- script.js
+++ script.js
@@ -4,18 +4,26 @@ const naudaKopa = 1000000;
 let uzvaretajuSkaits = 3;
 let rindas = document.querySelector('.rindas');
 //rindas.innerHTML = 'Hello';
-
+function randFunc(maxCipars)
+{
+    let random1=Math.floor(Math.random()*maxCipars);
+    return random1;
+}
+function izlozet()
+{
+    rindas.innerHTML=" ";
 for (let i = 0; i < uzvaretajuSkaits; i++) {
-    let rand = Math.random() * vardi.length;
-    rand = Math.floor(rand);
+    let rand = randFunc(vardi.length);
 
 
     let uzvaretajs=vardi[rand];
     rindas.innerHTML += `
     <tr>
-        <td>${i+1}</td>
+        <td>${i+1}</td> 
         <td>${uzvaretajs}</td>
     </tr>`
 
 
 }
+}
+//beidzas izloze
