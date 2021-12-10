00f59bb2f807c7ece732c112362d131950570af5
skolotaja <maiga.pigita@kkv.lv>
2021-12-10 12:21:44 +0200

    stundai

--- script.js
+++ script.js
@@ -1,33 +1,104 @@
-const vardi = ['Jana Krūmiņa', 'Jānis Bērziņš', 'Pēteris Liepa', 'Laura Ose', 'Artis Priede','Liene Ozola'];
-const balvas = ['Dators', 'Telefons', 'Ledusskapis', 'Fotoaparāts', 'Kafijas automāts'];
-const naudaKopa = 1000000;
-let uzvaretajuSkaits = 3;
-let rindas = document.querySelector('.rindas');
-//rindas.innerHTML = 'Hello';
-function randFunc(maxCipars)
+let tabulasRindas = document.querySelector('.rindas');
+let ciparuSkaits;
+let variantuSkaits;
+let papildusVards;
+
+const vardi1 = ['spalvains','karsta','papedis','mazais', 'pinkains'];
+const vardi2 = ['lacis','vista','ruksis','sunis','sesks','kakis'];
+const vardi3 = ['lien','klukst','lec','lido','skrien','naud'];
+
+
+
+function rand(maxCipars)
 {
-    let random1=Math.floor(Math.random()*maxCipars);
-    return random1;
+  random1 = Math.floor( Math.random() * maxCipars );
+  return random1;
 }
-function izlozet()
+
+
+
+function veidotParoli(papildusVards,ciparuSkaits)
 {
-    rindas.innerHTML=" ";
-for (let i = 0; i < uzvaretajuSkaits; i++) {
+ let random1 = rand(vardi1.length);
+ let random2 = rand(vardi2.length);
+ let random3 = rand(vardi2.length);
 
-    //let rand = Math.random() * vardi.length;​
-    //rand = Math.floor(rand);
-    let rand = randFunc(vardi.length);
+  let paroleVards1 = tekstuSarezgit( vardi1[random1] );
+  paroleVards1 = paroleVards1.replace('a','@');
+ 
+  let paroleVards2;
+
+  if( papildusVards != '' )
+  {
+    paroleVards2 = tekstuSarezgit( papildusVards );
+  }
+  else
+  {
+   paroleVards2 = tekstuSarezgit( vardi2[random2] );
+  }
+
+  let paroleVards3 = tekstuSarezgit( vardi3[random3] );
+
+  let cipari = cipariParolei(ciparuSkaits);
+
+  let parole = paroleVards1+paroleVards2+paroleVards3+cipari;
+  
+  return parole;
+}
 
 
 
-    let uzvaretajs = vardi[rand];
-    rindas.innerHTML += `
+function cipariParolei(ciparuSkaits)
+{
+   let cipars = String();
+
+   for (let i = 0; i < ciparuSkaits; i++) 
+   {
+     cipars += rand(9);
+   }
+   return cipars;
+}
+function tekstuSarezgit(teksts)
+{
+  if(rand(10) > 5)
+  {
+  teksts = teksts.replace('i','1');
+ teksts = teksts.replace('a','@');
+ }
+  teksts = lieloPirmoBurtu(teksts);
+
+  return teksts;
+}
+function lieloPirmoBurtu(vards)
+{
+  let pirmaisBurts = vards.substr(0, 1);
+  pirmaisBurts = pirmaisBurts.toUpperCase();
+  let parejaisVards = vards.substr(1);
+
+ return pirmaisBurts+parejaisVards;
+}
+
+
+
+function generetParoles()
+{
+  ciparuSkaits = document.querySelector('#ciparuSkaits').value;
+  variantuSkaits = document.querySelector('#variantuSkaits').value;
+  papildusVards = document.querySelector('#papildusVards').value;
+  tabulasRindas.innerHTML = '';
+  
+  for (let i = 0; i < variantuSkaits; i++) 
+  {
+    
+    
+    let parole = veidotParoli(papildusVards,ciparuSkaits);
+    
+    tabulasRindas.innerHTML += `
     <tr>
-        <td>${i + 1}</td>
-        <td>${uzvaretajs}</td> 
-    </tr>`
+    <td>${i+1}</td>
+    <td>${parole}</td>
+    </tr>`;
 
+  }//cikla beigas
 
-}
-}
-//beidzas izloze
+}//generetParoles() beigas
